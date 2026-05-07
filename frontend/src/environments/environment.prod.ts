2254f50d4192632b8f2e4e018c250cc734437dc0
Aaryaveersmarty <aaryaveersmarty@gmail.com>
2026-05-07 23:39:51 +0530

    fix: remove double slash from production API URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // We will configure this later with the real deployed backend URL
-  apiUrl: 'https://rawsource-api.onrender.com//api'
+  apiUrl: 'https://rawsource-api.onrender.com/api'
 };
